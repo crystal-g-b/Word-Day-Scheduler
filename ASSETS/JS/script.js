92b7d327056c475bc06a3b5d396a6b8c04c7c144
crystal-g-b <crystal.barnett1@gmail.com>
2021-03-07 00:20:50 -0500

    looked over the time block and Traversed the DOM to get values need in local storage

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,21 +1,55 @@
 var rowEl = $('#time-block');
 var timeEl = $('#hour');
+//get a ref to all of the save buttons $('.saveBtn)
+var saveEl = $('.saveBtn');
+var toDoEl =$('.description');
 
+//current day -> top of calendar
+    //use moment library - .format()
+    var today = moment();
+    $("#currentDay").text(today.format("MMMM Do, YYYY HH:mm:ss a"));
 
-//button
-    //gete a ref to all of thr save buttons $('.saveBtn)
-    //in activity 1 and 2
-    //add a click handler to each save button using .on() method- ref actvity 3 and 4
-    //traverse DOM to get values we need to storage in localstorage; need to access .parent() .sibling() - ref act 8
-    //set items in local storage -ref unit 4
+ //add a click handler to each save button using .on() method- ref actvity 3 and 4
+    saveEl.on('click', function() {
+ //traverse DOM to get values we need to storage in localstorage; need to access .parent() .sibling() - ref act 8
+        var toDoEl = $(this).siblings('.description').val();
+        var timeEl =$(this).parent().attr('id');
+//set items in local storage -ref unit 4
+       localStorage.setItem(timeEl, toDoEl);     
+    });
+    
 
 //handle color coding
-    //use the moment library to get the current hour 
-    //get a reference to all time blocks
-    //loop through time blocks- ref unit 3
+//use the moment library to get the current hour 
+    function colorCode() {
+        var currentHour = moment().hours();
+//get a reference to all time blocks
+ //loop through time blocks- ref unit 3
+        $('.time-block').each(function() {
+            var timeSlot = parseInt($(this).attr('.id').split('hour')[1]);
+//we need an if else statement- condition we compare the current hour with the timeblock " <, ===, >"- ref unit 3
+//adding or removing  (or both) classes
+            if (timeSlot < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (timeSlot === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+      
+    }
+
+
+    
+   
     //get the data hour value timeblocks[i].data('hour')
-    //we need an if else statement- condition we compare the current hour with the timeblock " <, ===, >"- ref unit 3
-    //adding or removing  (or both) classes
+    
 
 //get the data from localstorage and populate the timeblocks
     //getItem
@@ -25,8 +59,12 @@ var timeEl = $('#hour');
     // -> get a ref to id of .parent()
     //.val() = localStorage.getItem(pass in the parent id)
 
-//current day -> top of calendar
-    //use moment library - .format()
+
 
 //call the color coding function
+    colorCode();
+
 //setInterval -> 15000(every 15secs)
+
+saveEl.addEventListener("click", submitTasks);
+
